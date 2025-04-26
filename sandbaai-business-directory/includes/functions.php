999b08b648f888f976b89b058759ebeded576fc3
Christopher Reid <139740559+reidsart@users.noreply.github.com>
2025-04-27 01:46:10 +0200

    Update functions.php

--- sandbaai-business-directory/includes/functions.php
+++ sandbaai-business-directory/includes/functions.php
@@ -55,4 +55,32 @@ function sb_extend_business_search($query) {
 }
 add_action('pre_get_posts', 'sb_extend_business_search');
 
+// Add the business_tag taxonomy column to the Business Listing admin table
+function add_business_tag_column($columns) {
+    $columns['business_tag'] = 'Tags';
+    return $columns;
+}
+add_filter('manage_business_listing_posts_columns', 'add_business_tag_column');
+
+// Populate the business_tag column with terms
+function populate_business_tag_column($column, $post_id) {
+    if ($column === 'business_tag') {
+        $terms = get_the_terms($post_id, 'business_tag');
+        if (!empty($terms) && !is_wp_error($terms)) {
+            $tags = array_map(function($term) {
+                return $term->name;
+            }, $terms);
+            echo implode(', ', $tags);
+        } else {
+            echo 'No Tags';
+        }
+    }
+}
+add_action('manage_business_listing_posts_custom_column', 'populate_business_tag_column', 10, 2);
+
+function add_business_tag_metabox() {
+    register_taxonomy_for_object_type('business_tag', 'business_listing');
+}
+add_action('init', 'add_business_tag_metabox');
+
 ?>
